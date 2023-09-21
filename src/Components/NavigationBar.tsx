d86c592afe9c59180865a621430ac67221482ad2
jarkko2 <77615804+jarkko2@users.noreply.github.com>
2023-09-21 10:07:28 +0300

    Refactored navigationbar to typescript

--- src/Components/NavigationBar.tsx
+++ src/Components/NavigationBar.tsx
@@ -12,25 +12,62 @@ import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 
-const pages = ['job', 'study', 'projects'];
+type Page =
+  {
+    id: string;
+    friendlyName: string
+  }
 
-const pagesDictionary = {
-  job: 'Työhistoria',
-  study: 'Opinnot',
-  projects: 'Projektit',
-};
+const pages: Page[] = [
+  {
+    id: "job",
+    friendlyName: "Työhistoria"
+  },
+  {
+    id: "study",
+    friendlyName: "Opinnot"
+  },
+  {
+    id: "projects",
+    friendlyName: "Projektit"
+  }
+];
 
 function NavigationBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState<HTMLElement | null>(null);
 
-  const handleOpenNavMenu = (event) => {
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
   };
 
-  const handleCloseNavMenu = (page) => {
+  const handleCloseNavMenu = (page: string) => {
     window.location.hash = `#${page}`;
     setAnchorElNav(null);
   };
+
+
+  const PageList: React.FC<{ isMobile: boolean }> = ({ isMobile }) => {
+    return (
+      <>
+        {pages.map((page) => (
+          isMobile ? (
+            <MenuItem key={page.id} onClick={() => handleCloseNavMenu(page.id)}>
+              <Typography textAlign="center">{page.friendlyName}</Typography>
+            </MenuItem>
+          ) : (
+            <Button
+              key={page.id}
+              onClick={() => handleCloseNavMenu(page.id)}
+              sx={{ my: 2, color: 'white', display: 'block' }}
+            >
+              {page.friendlyName}
+            </Button>
+          )
+        ))}
+      </>
+    );
+  };
+
 
   return (
     <AppBar position="sticky">
@@ -65,23 +102,11 @@ function NavigationBar() {
                 display: { xs: 'block', md: 'none' },
               }}
             >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={() => handleCloseNavMenu(page)}>
-                  <Typography textAlign="center">{pagesDictionary[page]}</Typography>
-                </MenuItem>
-              ))}
+              <PageList isMobile={true} />
             </Menu>
           </Box>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={() => handleCloseNavMenu(page)}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {pagesDictionary[page]}
-              </Button>
-            ))}
+            <PageList isMobile={false} />
           </Box>
         </Toolbar>
       </Container>
